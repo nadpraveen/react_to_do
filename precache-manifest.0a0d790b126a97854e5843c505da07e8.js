b266d233450d059ff9307a7c090d30ed51259ba9
praveen <praveen.42000@gmail.com>
2020-05-26 21:54:58 +0530

    Updates

---
+++ precache-manifest.0a0d790b126a97854e5843c505da07e8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7b9dc71ef0c4b80f4431a2cb95a3c4",
+    "url": "/react_to_do/index.html"
+  },
+  {
+    "revision": "5ab1b9fc993da5b5ffd1",
+    "url": "/react_to_do/static/js/2.a5129320.chunk.js"
+  },
+  {
+    "revision": "80b81ae2cf4de0f8b202",
+    "url": "/react_to_do/static/js/main.be39e3b2.chunk.js"
+  },
+  {
+    "revision": "70ea703658885c1f6a7a",
+    "url": "/react_to_do/static/js/runtime-main.a5edb1ce.js"
+  }
+]);
